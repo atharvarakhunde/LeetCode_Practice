edc553c5195e3444dec41a5c8fddfd8859c24c3c
Atharva Rakhunde <92033678+atharvarakhunde@users.noreply.github.com>
2026-08-05 16:59:11 +0530

    Time: 2 ms (59.71%), Space: 43.2 MB (28.33%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> str1 = new Stack<>();
+        Stack<Character> str2 = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '#') {
+                if (!str1.isEmpty()) {
+                    str1.pop();
+                }
+            } else {
+                str1.push(s.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == '#') { 
+                if (!str2.isEmpty()) {
+                    str2.pop();
+                }
+            } else {
+                str2.push(t.charAt(i));
+            }
+        } 
+        
+        if (str1.size() != str2.size()) {
+            return false;
+        }
+
+        while (!str1.isEmpty()) {
+            if (str1.pop() != str2.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
