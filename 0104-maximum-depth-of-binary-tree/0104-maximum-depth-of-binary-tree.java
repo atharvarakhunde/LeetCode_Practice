8e2f71e80a979d503a287cc6575e13c8766d835c
Atharva Rakhunde <92033678+atharvarakhunde@users.noreply.github.com>
2026-08-16 13:07:40 +0530

    Time: 0 ms (100%), Space: 46.8 MB (64.41%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // Base case: if the tree is empty, the depth is 0
+        if (root == null) {
+            return 0;
+        }
+        
+        // Recursively find the depth of the left and right subtrees
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        
+        // The maximum depth is the greater of the two depths, 
+        // plus 1 to account for the current root node
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
